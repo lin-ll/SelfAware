5ac343c98e2b513e6f219ad3f60626e8586415aa
Lucy <ll7@princeton.edu>
2016-11-22 15:15:58 -0500

    More scheduling and starting navdrawer

--- app/src/main/java/ll7/secondapp/MainActivity.java
+++ app/src/main/java/ll7/secondapp/MainActivity.java
@@ -228,17 +228,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_home) {
+            // Handle the home action
+        } else if (id == R.id.nav_calls) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_sms) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_act) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_loc) {
 
         }
 
