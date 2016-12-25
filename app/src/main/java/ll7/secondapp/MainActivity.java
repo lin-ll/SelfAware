64e6aa845f7db9e570d24bf01546a7594c980056
Lucy <ll7@princeton.edu>
2016-12-25 16:45:01 -0600

    Map fragment shows up yay

--- app/src/main/java/ll7/secondapp/MainActivity.java
+++ app/src/main/java/ll7/secondapp/MainActivity.java
@@ -1,8 +1,12 @@
 package ll7.secondapp;
 
+import android.app.FragmentManager;
+import android.content.pm.PackageManager;
 import android.os.*;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.*;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -10,14 +14,24 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.FrameLayout;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
+
+    SupportMapFragment sMapFragment;
+    final private int myLocationId = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        sMapFragment = SupportMapFragment.newInstance();
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -34,6 +48,13 @@ public class MainActivity extends AppCompatActivity
         FragmentTransaction ft = getFragmentManager().beginTransaction();
         ft.replace(R.id.mainFrame, fragment);
         ft.commit();
+
+        sMapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        return;
     }
 
     @Override
@@ -66,29 +87,46 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        Fragment fragment = null;
+
+        FragmentManager fm = getFragmentManager();
+        android.support.v4.app.FragmentManager sfm = getSupportFragmentManager();
+
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_home) {
-            // Handle the home action
-            fragment = new FragmentHome();
-        } else if (id == R.id.nav_call_loc) {
-            fragment = new FragmentCallLoc();
-        } else if (id == R.id.nav_sms_loc) {
-            fragment = new FragmentCallLoc();
-        } else if (id == R.id.nav_call_act) {
-            fragment = new FragmentCallAct();
-        } else if (id == R.id.nav_sms_act) {
-            fragment = new FragmentCallAct();
-        }
+        if(sMapFragment.isAdded())
+            sfm.beginTransaction().hide(sMapFragment).commit();
 
-        FragmentTransaction ft = getFragmentManager().beginTransaction();
-        ft.replace(R.id.mainFrame, fragment);
-        ft.commit();
+        FrameLayout layout = (FrameLayout)findViewById(R.id.mainFrame);
+        layout.setVisibility(View.VISIBLE); // you can use INVISIBLE also instead of GONE
+
+        if (id == R.id.nav_home) {
+            helper(fm, new FragmentHome());
+        } else if (id == R.id.nav_call_loc) {
+            layout.setVisibility(View.GONE); // you can use INVISIBLE also instead of GONE
+            if(!sMapFragment.isAdded())
+                sfm.beginTransaction().add(R.id.map, sMapFragment).commit();
+            else
+                sfm.beginTransaction().show(sMapFragment).commit();
+        } else if (id == R.id.nav_sms_loc) {
+            layout.setVisibility(View.GONE); // you can use INVISIBLE also instead of GONE
+            if(!sMapFragment.isAdded())
+                sfm.beginTransaction().add(R.id.map, sMapFragment).commit();
+            else
+                sfm.beginTransaction().show(sMapFragment).commit();
+            helper(fm, new FragmentCallAct());
+        } else if (id == R.id.nav_call_act) {
+            helper(fm, new FragmentCallAct());
+        } else if (id == R.id.nav_sms_act) {
+            helper(fm, new FragmentCallAct());
+        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void helper(FragmentManager fm, Fragment fragment) {
+        fm.beginTransaction().replace(R.id.mainFrame, fragment).commit();
     }
 }
