77f28f85fab3d5188b74c2b53646a9393c0a1702
Lucy <ll7@princeton.edu>
2016-12-25 19:43:25 -0600

    Zooms to current location upon startup

--- app/src/main/java/ll7/secondapp/MainActivity.java
+++ app/src/main/java/ll7/secondapp/MainActivity.java
@@ -1,12 +1,9 @@
 package ll7.secondapp;
 
 import android.app.FragmentManager;
-import android.content.pm.PackageManager;
-import android.os.*;
+import android.location.Location;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.view.*;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,21 +12,43 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
+        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
-    SupportMapFragment sMapFragment;
-    final private int myLocationId = 123;
+    SupportMapFragment smsMap;
+    SupportMapFragment callMap;
+    GoogleMap mMap;
+    GoogleApiClient mGoogleApiClient;
+    Location mLastLocation;
+    LatLng currLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        sMapFragment = SupportMapFragment.newInstance();
+        smsMap = SupportMapFragment.newInstance();
+        callMap = SupportMapFragment.newInstance();
+
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -43,26 +62,47 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        navigationView.getMenu().getItem(0).setChecked(true);
 
         Fragment fragment = new FragmentHome();
         FragmentTransaction ft = getFragmentManager().beginTransaction();
         ft.replace(R.id.mainFrame, fragment);
         ft.commit();
 
-        sMapFragment.getMapAsync(this);
+        smsMap.getMapAsync(this);
+        callMap.getMapAsync(this);
+
+        buildGoogleApiClient();
+        if(mGoogleApiClient!= null)
+            mGoogleApiClient.connect();
+        else Toast.makeText(this, "Not connected...", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onMapReady(GoogleMap map) {
-        return;
+        mMap = map;
+
+        if (currLocation != null)
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(currLocation));
+        else {
+            LatLng princeton = new LatLng(40.3440, 74.6514);
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(princeton));
+        }
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+
+        mMap.getUiSettings().setCompassEnabled(true);
+        mMap.getUiSettings().setIndoorLevelPickerEnabled(false);
+        mMap.getUiSettings().setMapToolbarEnabled(false);
+        mMap.getUiSettings().setRotateGesturesEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
     }
 
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
+        if (drawer.isDrawerOpen(GravityCompat.START))
             drawer.closeDrawer(GravityCompat.START);
-        } else super.onBackPressed();
+        else super.onBackPressed();
     }
 
     @Override
@@ -94,31 +134,33 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if(sMapFragment.isAdded())
-            sfm.beginTransaction().hide(sMapFragment).commit();
+        if (smsMap.isAdded())
+            sfm.beginTransaction().hide(smsMap).commit();
+        if (callMap.isAdded())
+            sfm.beginTransaction().hide(callMap).commit();
 
         FrameLayout layout = (FrameLayout)findViewById(R.id.mainFrame);
         layout.setVisibility(View.VISIBLE); // you can use INVISIBLE also instead of GONE
 
         if (id == R.id.nav_home) {
-            helper(fm, new FragmentHome());
+            fragmentReplacer(fm, new FragmentHome());
         } else if (id == R.id.nav_call_loc) {
             layout.setVisibility(View.GONE); // you can use INVISIBLE also instead of GONE
-            if(!sMapFragment.isAdded())
-                sfm.beginTransaction().add(R.id.map, sMapFragment).commit();
+            if(!callMap.isAdded())
+                sfm.beginTransaction().add(R.id.map, callMap).commit();
             else
-                sfm.beginTransaction().show(sMapFragment).commit();
+                sfm.beginTransaction().show(callMap).commit();
         } else if (id == R.id.nav_sms_loc) {
             layout.setVisibility(View.GONE); // you can use INVISIBLE also instead of GONE
-            if(!sMapFragment.isAdded())
-                sfm.beginTransaction().add(R.id.map, sMapFragment).commit();
+            if(!smsMap.isAdded())
+                sfm.beginTransaction().add(R.id.map, smsMap).commit();
             else
-                sfm.beginTransaction().show(sMapFragment).commit();
-            helper(fm, new FragmentCallAct());
+                sfm.beginTransaction().show(smsMap).commit();
+            fragmentReplacer(fm, new FragmentCallAct());
         } else if (id == R.id.nav_call_act) {
-            helper(fm, new FragmentCallAct());
+            fragmentReplacer(fm, new FragmentCallAct());
         } else if (id == R.id.nav_sms_act) {
-            helper(fm, new FragmentCallAct());
+            fragmentReplacer(fm, new FragmentCallAct());
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -126,7 +168,32 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-    public void helper(FragmentManager fm, Fragment fragment) {
+    public void fragmentReplacer(FragmentManager fm, Fragment fragment) {
         fm.beginTransaction().replace(R.id.mainFrame, fragment).commit();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult arg0) {
+        Toast.makeText(this, "Failed to connect...", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onConnected(Bundle arg0) {
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null)
+            currLocation = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+    }
+
+    @Override
+    public void onConnectionSuspended(int arg0) {
+        Toast.makeText(this, "Connection suspended...", Toast.LENGTH_SHORT).show();
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
     }
 }
